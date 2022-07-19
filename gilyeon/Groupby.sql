b4ad25dfc970bac05cbecab30af04cad4839d262
gilyeon <kil2852@naver.com>
2022-07-19 21:34:44 +0900

    7/19 알고리즘

--- gilyeon/Groupby.sql
+++ gilyeon/Groupby.sql
@@ -11,4 +11,19 @@ group by NAME
 having count(NAME) >= 2
 order by NAME asc;
 
+-- 입양 시각 구하기(1)
+SELECT HOUR(DATETIME) HOUR, COUNT(DATETIME) COUNT
+FROM ANIMAL_OUTS
+GROUP BY HOUR(DATETIME)
+HAVING HOUR BETWEEN 9 AND 19
+ORDER BY HOUR ASC;
+
+-- 입양 시각 구하기(2)
+SET @hour := -1; -- 변수 선언
+
+SELECT (@hour := @hour + 1) as HOUR, 
+(SELECT COUNT(DATETIME) FROM ANIMAL_OUTS WHERE HOUR(DATETIME) = @hour) as COUNT
+FROM ANIMAL_OUTS
+WHERE @hour < 23;
+
 
